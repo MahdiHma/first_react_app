b61f2c20cc3b29e2a7dc456d62516f082808c9fb
mahdihma <mahdi.haji@yahoo.com>
2020-03-08 16:30:09 +0330

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Main from "./components/Main";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const li = ["bonjour", "ca va?"];
+const title = "Hello World";
+
+ReactDOM.render(<Main title={title} li={li}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
