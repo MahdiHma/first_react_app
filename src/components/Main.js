c2fa59b54c40d0891052545e848a84aa70375375
mahdihma <mahdi.haji@yahoo.com>
2020-03-11 22:54:33 +0330

    photos added to photowall

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react"
-import List from "./List";
 import Title from "./Title";
 import PhotoWall from "./PhotoWall";
 
@@ -7,7 +6,6 @@ class Main extends Component {
     render() {
         return <div>
             <Title title={this.props.title}/>
-            {/*<List li={this.props.li}/>*/}
             <PhotoWall photos={this.props.photos}/>
         </div>
     }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,7 +2,18 @@ import React, {Component} from "react";
 
 class Photo extends Component {
     render() {
+        const photo = this.props.photo;
         return <figure className={"figure"}>
+            <img className={"photo"} src={photo.imageLink} alt={photo.description}/>
+            <figcaption>
+                <p>
+                    {photo.description}
+                </p>
+                <div>
+                    <button> Remove</button>
+                </div>
+
+            </figcaption>
         </figure>
     }
 
